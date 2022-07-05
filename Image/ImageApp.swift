ecc2ee591033f8ea7c3a9e9bbabaa10bb9e3b8e5
kamrat97 <44671712+kamrat97@users.noreply.github.com>
2022-07-05 16:08:45 +0700

    Initial Commit

---
+++ Image/ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageApp.swift
+//  Image
+//
+//  Created by Влад on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
